4a847faa427195d6fd4d1641fac63417e92714f1
Juliluli <57632065+Juliluli@users.noreply.github.com>
2022-01-18 20:32:02 +0100

    version7

--- src/Components/Send/FormularioPago/FormularioPago.js
+++ src/Components/Send/FormularioPago/FormularioPago.js
@@ -1,27 +1,15 @@
-import React, { useEffect, useState, createContext} from 'react'
-import { useForm} from "react-hook-form";
+import { useState} from 'react'
 import styles from "./formulariopago.module.css";
-import { Route, Routes, useNavigate } from "react-router-dom";
-import NavButton from '../../NavButton/NavButton';
 import dotpattern from '../Images/DotPattern.svg';
 import reactangulo from '../Images/Rectangle.png';
-import NavBar from '../../NavBar/NavBar';
-import ConfirmacionPago from '../ConfirmacionPago/ConfirmacionPago';
-import RevisarDatos from '../RevisarDatos/RevisarDatos';
-export const ContextApp=createContext([])
-
 
 
 const FormularioPago = ({setPago, setChange}) => {
     
 
-    const [submitting, setSubmitting] = useState(false);
-
-
     const [email, setEmail] = useState('');
     const [amount, setAmount] = useState(0);
 
-    //const { register, handleSubmit, watch, formState: { errors } } = useForm();
 
     const onSubmit = () => {
         const data={
@@ -38,36 +26,13 @@ const FormularioPago = ({setPago, setChange}) => {
         //     },
         //     body: JSON.stringify(data)
         // });
-        // setSubmitting(true);
     }
 
-    // new Promise((resolve, reject)=>{
-    //     setTimeout(() => {resolve(items)}, 2000)})
-    //     .then(resp => setProductos(resp))
-    //     .catch(err=>console.log(err))
-    //     .finally(()=>setLoading(false))
 
-    setTimeout(() => {
-        setSubmitting(false);
-      }, 1500)
-    
 
-    //console.log(watch("name")); // watch input value by passing the name of it
-    //console.log(watch(data)); 
-    // console.log(watch(handleSubmit('email')))
-    // console.log(watch(email))
-
-    // useEffect(() => {
-    //     console.log('Errors', errors);
-    // }, [errors])
-
-    const navigate = useNavigate();
     
     return (
         
-        // <div className={styles.mainpago}>
-        //     <ContextApp.Provider value={{valores, setValores}}> 
-        //     <NavBar/> 
             <div className={styles.formulariopago}>
                 
                 <img  src={dotpattern} alt="dashboardlogo" className={styles.dotpattern}/>
@@ -77,24 +42,18 @@ const FormularioPago = ({setPago, setChange}) => {
                 <form className={styles.formulario} onSubmit={()=>{onSubmit()}}>
                     <label className={styles.shadow}>
                     <input type="email" placeholder='Email' value={email} onChange={(e)=>{setEmail(e.target.value)}} className={styles.input}/>
-                    {/* {errors.email?.type === 'required' && <p className={styles.error}>This field is required</p>}
-                    {errors.email?.type === 'pattern' && <p className={styles.error}>Incorrect Email</p>} */}
+
                     </label>
                     <br/>
                     <label>
                     <input type="number" placeholder='Amount' value={amount} onChange={(e)=>{setAmount(e.target.value)}} className={styles.input}/>
-                    {/* {errors.amount?.type === 'required' && <p className={styles.error}>This field is required</p>}
-                    {errors.amount?.type === 'pattern' && <p className={styles.error}>Incorrect amount</p>} */}
+ 
                     </label>
                     <br/>
-                    <input type="submit" className={styles.submit} onClick={() => {onSubmit()}}
-                    />
-                    {/* <div className={styles.estilohome} onClick={() => {navigate("/main/dashboard")}}><NavButton path = "/" text="Submit"/></div> */}
-                   {submitting &&<div>Submtting Form...</div>}
+                    <input type="submit" className={styles.submit} onClick={() => {onSubmit()}}/>
                 </form>
             </div>
-        //     </ContextApp.Provider>
-        // </div>
+
     )
 }
 
--- src/Components/Send/RevisarDatos/RevisarDatos.js
+++ src/Components/Send/RevisarDatos/RevisarDatos.js
@@ -1,25 +1,11 @@
-import React, {useContext} from 'react'
 import styles from "./revisardatos.module.css";
-import { useNavigate } from "react-router-dom";
-
-import { ContextApp } from '../FormularioPago/FormularioPago';
-import NavBar from '../../NavBar/NavBar';
-import NavButton from '../../NavButton/NavButton';
 import dotpattern from '../Images/DotPattern.svg'
 import reactangulo from '../Images/Rectangle.png';
 
 const RevisarDatos = ({setChange, pago}) => {
-    const navigate = useNavigate();
-    //const { register, handleSubmit  } = useFormContext(){handleSubmit("name")}
-    //console.log(register)
-    const { formValues, setFormValues} = useContext(ContextApp);
 
-    console.log(formValues)
-    
     return (
-        // <>
-        // <div className={styles.mainpago}>
-        //     <NavBar/>
+
             <div className={styles.confirmacionpago}>
                 <img  src={dotpattern} alt="dashboardlogo" className={styles.dotpattern}/>
                 <img  src={reactangulo} alt="dashboardlogo" className={styles.rectangulo}/>
@@ -28,14 +14,9 @@ const RevisarDatos = ({setChange, pago}) => {
                 <br/>
                 <h2 className={styles.texto1}>Amount: {pago.amount}</h2>
                 <br/>
-                
-
-                {/* <div className={styles.estilohome}><NavButton path = "/main/dashboard" text="Volver al Dashboard"/></div> */}
-                <input type="submit" className={styles.submit} onClick={()=> setChange(2)}/>
+               <input type="submit" className={styles.submit} onClick={()=> setChange(2)}/>
             </div>
-            
-        // </div>
-        // </>
+
     )
 }
 
