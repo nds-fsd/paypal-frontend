5ed390fbc90b39a05a5b9fdfc73d67eb4951c6db
Marc Sanchez Civantos <marc.sanchez.civantos@gmail.com>
2022-01-19 23:33:36 +0100

    Ajuste css confirmacion pago

--- src/Components/Send/ConfirmacionPago/ConfirmacionPago.js
+++ src/Components/Send/ConfirmacionPago/ConfirmacionPago.js
@@ -7,11 +7,13 @@ import reactangulo from '../Images/Rectangle.png';
 const ConfirmacionPago = ({setChange}) => {
     return (
         <div className={styles.confirmacionpago}>
-            
             <img  src={dotpattern} alt="dashboardlogo" className={styles.dotpattern}/>
-            <img  src={reactangulo} alt="dashboardlogo" className={styles.rectangulo}/>
-            <h1 className={styles.titulo}>Confirmación de Pago</h1>
-            <input type='button' value='Done' className={styles.submit} onClick={()=> setChange(0)}/>
+            {/* <img  src={reactangulo} alt="dashboardlogo" className={styles.rectangulo}/> */}
+            <div className = {styles.rectangulo}>
+                <h1 className={styles.titulo}>Confirmación de Pago</h1>
+                <input type='button' value='Done' className={styles.submit} onClick={()=> setChange(0)}/>
+            </div>
+            
         </div>
     )
 }
