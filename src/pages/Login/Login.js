0db1d0b2e6db6fd6eb033d8a6ab27cc4caf5b446
MaryLizR <marylizr@gmail.com>
2022-01-31 13:07:57 +0100

    log-in

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,13 +1,64 @@
 import styles from "./login.module.css"
-import NavButton from '../../Components/NavButton/NavButton'
+import customFetch from '../../api';
+import { setUserSession, getUserToken } from "../../api/auth";
+import { useForm } from "react-hook-form";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import imageLog from "../../assets/imageLog.png";
+import paydayLogo from "../../assets/paydayLogo.png";
+import { Link } from 'react-router-dom';
+
+
 
 const Login = () => {
-    return(
-        <div className={styles.login}>
-            Login
-            <NavButton path="/" text="Go Back"/>
-        </div>
-    )
-}
+    const navigate = useNavigate();
 
-export default Login;
+    useEffect(() => {
+        const token = getUserToken()
+        if (token) navigate("/dashboard");
+      }, []);
+    
+      const { register, handleSubmit, formState: { errors } } = useForm();
+    
+      const onSubmit = (data) => {
+        customFetch("POST", "login", {body: data})
+        .then(userSession => {
+          setUserSession(userSession);
+          navigate("/dashboard");
+        }).catch(error => {
+            'REQUEST_FAILED'
+          console.error(error);
+        });
+      };
+    
+      return (
+        <div className={styles.login}>
+            <div className={styles.login_block}>
+                <div className={styles.logo}>
+                    <img src={paydayLogo} alt="" />
+                </div>
+                <div className={styles.login_inputs}>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <input placeholder="email" {...register("email", {required: true })}/>
+                        {errors.email && <span className={styles.error}>email field is required</span>}
+                        
+                        <input placeholder="password"{...register("password", { required: true })} />
+                        {errors.password && <span>password field is required</span>}
+                        
+                        <div className={styles.send_button}>
+                            <input type="submit" value="Login" />
+                        </div>
+                    </form>
+                    <p>Don´t have an account? <Link className={styles.link} to='/register'>Sign up</Link></p>
+                </div>
+            </div>
+             
+            <div className={styles.block_img}>
+                <img src={imageLog} alt="" />
+            </div>
+            
+        </div>
+      );
+    };
+    
+    export default Login;
