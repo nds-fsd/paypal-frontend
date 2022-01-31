e8a1d1c2400a271e7392aeb9c5047fb8a718126c
nicocoll <nicocollpachan@gmail.com>
2022-01-31 20:38:39 +0100

    Register form

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -1,11 +1,22 @@
 import styles from "./register.module.css"
 import NavButton from '../NavButton/NavButton'
+import { useState} from 'react'
+import RegisterPart1 from "./RegisterPart1/RegisterPart1"
+import RegisterPart2 from "./RegisterPart2/RegisterPart2"
 
-const Register = () => {
+const Register = ({setCahnge}) => {
+    
+    const [change, setChange] = useState(0)
+    const onSubmit = () => {
+        setChange(1)
+    }
+
+
     return(
-        <div className = {styles.register}>
-            Register
-            <NavButton path="/" text="Go Back"/>
+        <div className = {styles.send}>
+            {change===0 && <RegisterPart1 setChange={setChange}/>}
+            {/* {change===1 && <RegisterPart2 setChange={setChange}/>} */}
+            {/* {change===2 && <RegisterConfirmation setChange={setChange}/>} */}
         </div>
     )
 }
