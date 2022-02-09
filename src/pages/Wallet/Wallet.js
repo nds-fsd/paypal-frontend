6e73930afa291b61ed9cce2f19ff0efec1ee2d00
Marc Sanchez Civantos <marc.sanchez.civantos@gmail.com>
2022-02-09 17:32:59 +0100

    Maquetar wallet

--- src/pages/Wallet/Wallet.js
+++ src/pages/Wallet/Wallet.js
@@ -1,9 +1,28 @@
 import styles from "./wallet.module.css"
+import dotpattern from "../../assets/DotPattern3.png"
 
 const Wallet = () => {
     return(
         <div className = {styles.wallet}>
-            Wallet
+            <img className={styles.dot1}src = {dotpattern} alt="dotpattern"></img>
+            <div className={styles.container}>
+                <div className={styles. bar}></div>
+                <div className={styles.addFunds}>
+                    <div className={styles.submit}>
+                        <p className={styles.fundText}>Add Funds</p>
+                    </div>
+                </div>
+                <div className={styles.percent}>23%</div>
+                <div className = {styles.money}>269.89$</div>
+                <div className = {styles.subTitle}>Recent transactions</div>
+                <div className = {styles.options}>
+                    <p className={styles.all}>All</p>
+                    <p className={styles.income}>Income</p>
+                    <p className={styles.outcome}>Outcome</p>
+                </div>
+
+                <div className={styles.show}>Show more transactions</div>
+            </div>
         </div>
     )
 }
