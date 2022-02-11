9969a3908541d9006681e5095a9d298e0bd96277
Marc Sanchez Civantos <marc.sanchez.civantos@gmail.com>
2022-02-11 20:16:42 +0100

    request

--- src/pages/Send/FormularioPago/FormularioPago.js
+++ src/pages/Send/FormularioPago/FormularioPago.js
@@ -1,21 +1,49 @@
 import styles from "./formulariopago.module.css"
-import NavButton from "../../NavButton/NavButton"
+import { useState} from 'react'
+import dotpattern from '../Images/DotPattern.svg';
+import customFetch from '../../../api'
 
 
-const FormularioPago = () => {
+const FormularioPago = ({setPago, setChange, pago}) => {
+
+
+    const [email, setEmail] = useState(pago.email);
+    const [amount, setAmount] = useState(pago.amount);
+    const [err, setErr] = useState("");
+
+
+    const onSubmit = () => {
+        
+        customFetch("GET", "users/id/" + email)
+        .then((_id) => {
+            if(_id === null) setErr("Email not found")
+            else {
+                const data={
+                    email:email,
+                    amount:amount,
+                    id:_id
+                }
+                setPago(data);
+                setChange(1);
+            }
+        })
+    }
+
+
     return(
         <div className = {styles.send}>
+            <img  src={dotpattern} alt="dashboardlogo" className={styles.dotpattern}/>
             <div className = {styles.paybox}>
                 <form className= {styles.form}>
                     <p>Send money to another user</p>
-                    <br></br>
-                    <input className = {styles.email} type="email" id="email" name="email" placeholder="Receiver's Email"></input>
-                    <br></br>
-                    <input className = {styles.amount} type="number" id="amount" name="amount" placeholder="Amount"></input>
-                    <br></br>        
-                    {/* <NavButton className = {styles.submit} path = "/main/send/paymentcheck" text="Transfer founds"/>       */}
-                    <input className = {styles.submit} type="submit" id="submit" name="submit" value="Transfer founds"></input>
+                    <br/>
+                    <input type="email" placeholder='Email' value={email} onChange={(e)=>{setEmail(e.target.value)}} className={styles.input}/>
+                    <br/>
+                    <input type="number" placeholder='0' value={amount} onChange={(e)=>{setAmount(e.target.value)}} className={styles.input}/>
+                    <br/>       
+                    <button type="button" className={styles.submit} onClick={() => {onSubmit()}}>Send</button>
                 </form>
+                {err}
             </div>
         </div>
     )
