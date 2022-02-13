bdd199787d41b3251ff53ab55c1306daaff109e7
Juliluli <57632065+Juliluli@users.noreply.github.com>
2022-02-13 22:36:38 +0100

    mejorar el diseño

--- src/pages/AccountSetting/AccountSetting.js
+++ src/pages/AccountSetting/AccountSetting.js
@@ -1,23 +1,20 @@
-import styles from "./accountsetting.module.css"
+import styles from "./settings.module.css"
 import { useForm } from "react-hook-form";
-import { useEffect , useState} from "react";
+import { useEffect , useState, useRef} from "react";
 import userimg from './images/userimg.svg'
 import pen from './images/pen.svg'
 import { useNavigate } from "react-router-dom";
 import customFetch from '../../api';
-import { setUserSession, getUserToken ,getSessionUser} from "../../api/auth";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye } from "@fortawesome/free-solid-svg-icons";
 const eye = <FontAwesomeIcon icon={faEye} />;
-
 
 //Instalaciones:
 //npm i react-hook-form
 // npm i --save @fortawesome/fontawesome-svg-core
 // npm i --save @fortawesome/free-solid-svg-icons
 // npm i --save @fortawesome/react-fontawesome
-
 
 const AccountSetting = () => {
 
@@ -29,8 +26,7 @@ const AccountSetting = () => {
     };
 
     ///TEST VARIABLE TO SEE THE INPUT///////
-    const test1='Test'
-
+ 
     const [userData, setUserData] = useState({
       name:'', surname:'', email:'', password:''
     })
@@ -52,70 +48,31 @@ const AccountSetting = () => {
 
     useEffect(() => {
         getUser();
-      }, []);
+    }, []);
 
     
-      const getUser = () => {
-        const userSession = localStorage.getItem("user-session");
-        const { token } = JSON.parse(userSession);
-        fetch("http://localhost:3090/users/me" , {
-          method: "GET",
-          headers: { Authorization: "Bearer " + token },
-          })
-          .then((response) => {
-          if (response.status !== 200) throw new Error("Couldn't retrieve user data");
-          return response.json();
-          })
-          .then((json) => {
-            setUser(json);
-          });
-          }
-        
+    const getUser = () => {
+      const userSession = localStorage.getItem("user-session");
+      const { token } = JSON.parse(userSession);
+      fetch("http://localhost:3090/users/me" , {
+        method: "GET",
+        headers: { Authorization: "Bearer " + token },
+        })
+        .then((response) => {
+        if (response.status !== 200) throw new Error("Couldn't retrieve user data");
+        return response.json();
+        })
+        .then((json) => {
+          setUser(json);
+        });
+    }
 
+        
     const {
         register,
         handleSubmit,
         formState: { errors },
     } = useForm();
-
-    const onSubmit = (data) => {
-        customFetch("PUT", "users", {body: data})
-        .then(userSession => {
-          setUserSession(userSession);
-          navigate("/main/dashboard");
-        }).catch(error => {
-          // Ideally, we should show an error message to the user
-          console.error(error);
-        });
-      };
-
-      const onSuubmit = () => {
-        const userSession = localStorage.getItem("user-session");
-        const { token } = JSON.parse(userSession);
-
-        const data = {
-          name: userData.name,
-          surname: userData.surname,
-          email: userData.email,
-          password: userData.password
-      }
-      // customFetch("PATCH", "users", {body:data})
-        
-        fetch("http://localhost:3090/users/me" , {
-          method: "PUT",
-          headers: { Authorization: "Bearer " + token },
-          body:data
-          })
-          .then((response) => {
-          if (response.status !== 200) throw new Error("Couldn't update user data");
-          return response.json();
-          })
-          .then((json) => {
-            setUser(json);
-          });
-
-    }
-
 
     const onSuuubmit = () => {
       const userSession = localStorage.getItem("user-session");
@@ -126,56 +83,84 @@ const AccountSetting = () => {
         email: userData.email,
         password: userData.password
       }
-      customFetch("PUT", "users/" + id , {body:data})
-  }
+      customFetch("PUT", "users/" + id , {body:data});
+      navigate("/main/dashboard");
+    }
+
+    
+    const inputFile = useRef(null);
+    const [photos, setPhotos] = useState([]);
+
+    const addPhoto = (data) => {
+      setPhotos([...photos, data])
+    };
+
+    const onUpload = () => {
+        debugger;
+        const files = inputFile.current.files;
+        const formData = new FormData();
+        const url = "https://api.cloudinary.com/v1_1/???????/image/upload";
+      
+        for (let i = 0; i < files.length; i++) {
+          let file = files[i];
+          formData.append("file", file);
+          formData.append("upload_preset", "hv3kmdau");
+          fetch(url, {
+            method: "POST",
+            header: {
+                'Content-Type': 'multipart/form-data'
+            },
+            body: formData
+          })
+            .then((response) => {
+                console.log(response);
+              return response.text();
+            })
+            .catch((data) => {
+              debugger;
+              addPhoto(JSON.parse(data));
+              console.log(data);
+            });
+        }
+    };
 
     return(
+
         <div className = {styles.settings}>
-            <div className = {styles.settingsbox}>
-                <p>Edit profile</p>
-
-                <div className="images">
-
-                <div className={styles.userimage}><img  src={userimg} alt="userImage"/></div>
-                <div className={styles.editimg}><img  src={pen} alt="penlogo"/></div>
-                
-                </div>
-                
-                
-                <form onSubmit={handleSubmit(onSuuubmit)} onChange={handleChange} className= {styles.form}>
-                    <br></br>
+            <div className = {styles.editbox}>
+                <form className= {styles.form} onSubmit={handleSubmit(onSuuubmit)} onChange={handleChange}>
+                    <p>Edit profile</p>
                     
+                    <div className={styles.images}>
+                    <div className={styles.userimage}><img src={userimg} alt="userImage"/></div>
+                    <div className={styles.editimg}>
+                      <img src={pen} alt="penlogo"/>
+                      <input type='file' ref={inputFile} className={styles.uploading}></input></div>
+                    </div>
+
+                    <br></br>
+
                     <div className= {styles.namesinput}>
-                        <input className = {styles.names} type="name" id="name" name="name" placeholder={user.name} {...register("name")}></input>
-                        {/* {errors.name && <span>This field is required</span>} */}
-                        <input className = {styles.names} type="surname" id="surname" name="surname" placeholder={user.surname} {...register("surname")}></input>
-                        {/* {errors.surname && <span>This field is required</span>} */}
+                      <input className = {styles.names} type="name" id="name" name="name" placeholder={user.name} {...register("name")}></input>
+                      <input className = {styles.names} type="surname" id="surname" name="surname" placeholder={user.surname} {...register("surname")}></input>
                     </div>
-                    <br></br>
 
-                    
+                    <br></br>
                     <input className = {styles.email} type="email" id="email" name="email" placeholder={user.email} {...register("email")}></input>
-                    {errors.email && <span>This field is required</span>}
-                    
                     
                     <br></br>
 
-                    <div className="password-wrapper">
-                    <input type={passwordShown ? "text" : "password"} id="password" name="password" placeholder='Password' {...register("password")}></input>
-                    {errors.password && <span>This field is required</span>}
-                    <i onClick={togglePasswordVisiblity}>{eye}</i>
+                    <div class={styles.passwordeye}>
+                      <input className = {styles.password} type={passwordShown ? "text" : "password"} id="password" name="password" placeholder='Password' {...register("password")} />
+                      <i className={styles.eye} onClick={togglePasswordVisiblity}>{eye}</i>
                     </div>
 
-                    <br></br>      
-
-                    <div>
-                    <input className = {styles.save} type="submit" id="submit" name="submit" value="Save"></input>
-                    </div>  
-
-                    <h1>Hello {register.surname}</h1>
+                    <br></br>        
+                    <input className = {styles.submit} type="submit" id="submit" name="submit" value="Save"></input>
                 </form>
-            </div>
         </div>
+    </div>
+
     )
 }
 
