93aec2329e97cdcd2d4db57ac2fe823d2b870de2
MaryLizR <marylizr@gmail.com>
2022-03-10 15:03:49 +0100

    barchart

--- src/Components/linechart/Linechart.js
+++ src/Components/linechart/Linechart.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BarChart, Bar, XAxis, YAxis } from 'recharts';
+import { BarChart, Bar, XAxis, YAxis, Tooltip, Legend } from 'recharts';
 import styles from '../linechart/linechart.module.css';
 import { UserContext } from "../../context/userContext";
 import { useContext, useEffect, useState } from "react";
@@ -12,50 +12,55 @@ const RenderLineChart = () => {
    const { payments, setPayments } = useContext(UserContext);
    const [income, setIncome] = useState(0);
    const [outcome, setOutcome] = useState(0);
+   const [date, setDate] = useState;
 
    useEffect(() => {
-         const userSession = getStorageObject;
-         const { id } = JSON.parse(userSession);
-         customFetch("GET", "users/" + id +"/payments/")
+         const id = getStorageObject("id");
+   
+         customFetch("GET", "users/" + id +"/payments")
          .then(paymentsBack => {
              setPayments(paymentsBack);
          });
     
       const from = payments.map((payments) => {return payments.to === id ? null : payments});
       const to = payments.map((payments) => {return payments.from === id ? null : payments});
+      const paymentDate = payments.map((payments) => {return payments.paymentDate === id ? null : payments});
 
       let resultFrom = 0;
       from.forEach(element => {
           resultFrom+=element.amount;
       });
-      
+         setOutcome(resultFrom);
+         setDate(paymentDate);
+
       let resultTo = 0;
       to.forEach(element => {
           resultTo+=element.amount;
       });
+         setIncome(resultTo);
+         setDate(paymentDate);
 
-   }, [payments, setPayments])
+   }, [payments, setPayments, setDate])
    
 
       const data = [
          {
-            name: "Income", 
-            value: {income},
-         },
-         {
-            name: "Outcome", 
-            value: {outcome},
+            month:{date},
+            Income: {income},
+            Outcome: {outcome},
          },
       ];
   return (
       <div className={styles.linechart}> 
          <h2>your balance </h2>
          <BarChart width={300} height={300} data={data}>
-            <XAxis dataKey="name" />
+            <XAxis dataKey="month" />
             <YAxis />
-             
-                <Bar dataKey="value" barSize={50} fill="#20E9BC" />
-               
+            <Bar dataKey="Income" fill="#8884d8" />
+            <Bar dataKey="Outcome" fill="#FF374F" />
+            <Tooltip />
+            <Legend />
+  
          </BarChart>
    </div>
   )
